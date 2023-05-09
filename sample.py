1814c03b6a0262d718605fd1348ce8c869c90575
Andrew Ghorbani <andrewbghorbani@gmail.com>
2023-05-08 18:18:02 -0700

    clean up sample.py

--- sample.py
+++ sample.py
@@ -5,12 +5,10 @@ from src.trading_bots.simple_moving_average import TradeBotSimpleMovingAverage
 from src.trading_bots.twitter_sentiments import TradeBotTwitterSentiments
 from src.trading_bots.volume_weighted_average_price import TradeBotVWAP
 
-# Usage: python sample.py <company_ticker>
-
 
 def main():
     if len(sys.argv) != 2:
-        raise Exception("Must run as python sample.py [company_ticker]")
+        raise Exception("ERROR: Usage: python sample.py <company_ticker>")
 
     args = sys.argv[1:]
     ticker = args[0]
@@ -22,11 +20,7 @@ def main():
 
     print(f"Current positions : {tb0.get_current_positions()}")
     print(f"Current cash position : ${tb0.get_current_cash_position()}")
-
-    company_name = tb0.get_company_name_from_ticker(ticker)
-    print(f"Market price of {company_name} is ${tb0.get_current_market_price(ticker)}")
-
-    # Order Recommendations from the bots
+    print(f"Market price of {tb0.get_company_name_from_ticker(ticker)} is ${tb0.get_current_market_price(ticker)}")
     print(f"SimpleMovingAverage : {tb1.make_order_recommendation(ticker)}")
     print(f"VWAP : {tb2.make_order_recommendation(ticker)}")
     print(f"TwitterSentiments : {tb3.make_order_recommendation(ticker)}")
